162f32101eae94538ffb85ca07723d03b923cdd6
Daniel Esposito <danielesposito@macbkk.com>
2016-03-16 20:47:33 +0700

    Completed Stack View Tutorial

--- stackdamania/ViewController.swift
+++ stackdamania/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var shoe1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func hideShoe1(sender: UIImageView) {
+        
+        shoe1.hidden = true
+        
+    }
 }
 
